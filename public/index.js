26bccce79bc2d2250765698d2aa2b23201a692a2
Karim-CCTT-28 <karimhamza2003@gmail.com>
2025-03-24 23:57:41 +0200

    Add files via upload

---
+++ public/index.js
@@ -0,0 +1,90 @@
+var balance = 0 ,Dermatology = 0 , heart = 0, Medical_examination = 0 , Dentistry = 0;
+var flag = true ;
+
+for(let i=0 ; i<document.querySelectorAll(".box").length;i++){
+
+    document.querySelectorAll(".box")[i].addEventListener("click",function(){
+        document.querySelector(".form-2").style.display = "block";
+        console.log(i)
+        if(flag){
+
+        
+        switch(i){
+            case 0:
+                balance+=40;
+                Dermatology++;
+                console.log("here is it")
+            break;    
+
+            case 1:
+                balance+=200;
+                heart++;
+            break; 
+
+            case 2:
+                balance+=20;
+                Medical_examination++;
+            break; 
+
+            case 3:
+                balance+=80;
+                Dentistry++;
+            break; 
+        }
+        document.querySelector(".b span").textContent=balance;
+        document.querySelector("#A span").textContent=Dermatology;
+        document.querySelector("#B span").textContent=heart;
+        document.querySelector("#C span").textContent=Medical_examination;
+        document.querySelector("#D span").textContent=Dentistry;
+        flag=false;
+    }
+
+
+
+
+
+
+
+
+
+       
+    });
+}
+
+
+
+
+let close1 = document.querySelector("#close");
+close1.addEventListener("click",function(){
+    document.querySelector(".form-2").style.display = "none";
+
+});
+
+
+let download = document.querySelector("#download");
+download.addEventListener("click",function(){
+    document.querySelector(".form-2").style.display = "none";
+    flag=true;
+
+
+});
+
+
+let balanceButton = document.querySelector(".balance-button");
+balanceButton.addEventListener("click",function(){
+    console.log("you clicked on balnace button")
+    document.querySelector(".balance").style.display="flex";
+    document.querySelector(".close-balance-window").style.display = "block"
+    balanceButton.style.dsiplay = "none";
+
+
+});
+
+let close2 = document.querySelector(".close-balance-window");
+close2.addEventListener("click",function(){
+    console.log("you clicked on close button")
+
+    document.querySelector(".balance").style.display="none";
+    balanceButton.style.dsiplay = "block";
+    close2.style.display = "none";
+});
